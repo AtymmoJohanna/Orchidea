84d5230ccfeec083073517c9be5298703c67eab1
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2025-03-07 12:00:58 +0100

    modif des routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import StatView from "@/views/StatView.vue";
 import CameraView from "@/views/CameraView.vue";
+import ParamView from '@/views/ParamView.vue';
 
 const routes = [
   { path: "/", redirect: "/stats" },
   { path: "/stats", component: StatView },
   { path: "/camera", component: CameraView },
+  { path: "/param", component: ParamView },
 ];
 
 const router = createRouter({
