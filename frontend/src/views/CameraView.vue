42e51c634939e8749d52b16d2fb8bc0b682f9a29
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2025-03-07 09:11:31 +0100

    Ajout CameraView et statView

---
+++ frontend/src/views/CameraView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Identification de l'image</h1>
+    <CameraComponent />
+    <FooterVue />
+  </div>
+</template>
+
+<script>
+import FooterVue from "@/components/FooterVue.vue";
+import CameraComponent from '@/components/CameraComponent.vue'
+
+export default {
+  components: { CameraComponent, FooterVue },
+};
+</script>
---
+++ frontend/src/views/statView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Statistiques</h1>
+    <MapsComponent /> <!-- MapsVue fait partie de StatView -->
+    <FooterVue />
+  </div>
+</template>
+
+<script>
+import MapsComponent from "@/components/MapsComponent.vue";
+import FooterVue from "@/components/FooterVue.vue";
+
+export default {
+  components: {
+    MapsComponent,
+    FooterVue,
+  },
+};
+</script>
